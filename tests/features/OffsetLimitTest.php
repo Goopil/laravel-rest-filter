70c28201f9bfe08f3c03f5afff8e36f85ee70e5a
Zachary Volpi <z.volpi@infomir.com>
2018-03-19 20:33:57 +0100

    add some assert

--- tests/features/OffsetLimitTest.php
+++ tests/features/OffsetLimitTest.php
@@ -1,45 +1,51 @@
 <?php
+
 namespace Goopil\RestFilter\Tests\Features;
 
 use Goopil\RestFilter\Tests\BaseTestCase;
 
-class OffsetLimitTest extends BaseTestCase {
+class OffsetLimitTest extends BaseTestCase
+{
 
     /**
      * @test
      */
-    public function itShouldLimitTheEntitiesToTwo ()
+    public function itShouldLimitTheEntitiesToTwo()
     {
         $decoded = $this->callEndpoint(['limit' => 2]);
         $this->assertCount(2, $decoded);
+        $this->assertEquals(1, $decoded[0]['id']);
+        $this->assertEquals(2, $decoded[1]['id']);
     }
 
     /**
      * @test
      */
-    public function itShouldApplyTheDefaultLimitIfParamsIsNotValidInt ()
+    public function itShouldApplyTheDefaultLimitIfParamsIsNotValidInt()
     {
         $decoded = $this->callEndpoint(['limit' => '125']);
         $this->assertCount(15, $decoded);
+        $this->assertEquals(1, $decoded[0]['id']);
     }
 
     /**
      * @test
      */
-    public function itShouldApplyOffsetAndLimitAndReturnTheSecondEntities ()
+    public function itShouldApplyOffsetAndLimitAndReturnTheSecondEntities()
     {
         $decoded = $this->callEndpoint(['offset' => 1, 'limit' => 1]);
 
         $this->assertCount(1, $decoded);
+        $this->assertEquals(2, $decoded[0]['id']);
     }
 
     /**
      * @test
      */
-    public function itShouldApplyOffsetAndReturnAllStartingToSecond () {
+    public function itShouldApplyOffsetAndReturnAllStartingToSecond()
+    {
         $decoded = $this->callEndpoint(['offset' => 1]);
-
-        $this->assertNotEquals(1, $decoded[0]['id']);
+        $this->assertNotEquals(2, $decoded[0]['id']);
     }
 
 }

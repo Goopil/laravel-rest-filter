832b6fba30c7bcbc9427bfbd4c4294f1717fec70
Zachary Volpi <z.volpi@infomir.com>
2018-03-19 22:52:00 +0000

    Resolve "test[feature] filter scope"

---
+++ tests/features/FilterTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Goopil\RestFilter\Tests\Features;
+
+use Goopil\RestFilter\Tests\BaseTestCase;
+use Goopil\RestFilter\Tests\Utils\TestModel;
+
+class FilterTest extends BaseTestCase
+{
+    /**
+     * @test
+     */
+    public function ItShouldReturnOnlyTheFilteredField()
+    {
+        $models = TestModel::all('string');
+        $decoded = $this->callEndpoint(['filter' => 'string']);
+
+        $this->assertEquals($models->toArray(), $decoded);
+    }
+
+    /**
+     * @test
+     */
+    public function ItShouldReturnOnlyTheMultipleFilteredFieldsWithParamAsArray()
+    {
+        $models = TestModel::all(['string', 'text']);
+        $decoded = $this->callEndpoint(['filter' => ['string', 'text']]);
+
+        $this->assertEquals($models->toArray(), $decoded);
+    }
+
+    /**
+     * @test
+     */
+    public function ItShouldReturnOnlyTheMultipleFilteredFieldsWithParamAsStringSeparatedWithPrimary()
+    {
+        $models = TestModel::all(['string', 'text']);
+        $decoded = $this->callEndpoint(['filter' => 'string;text']);
+
+        $this->assertEquals($models->toArray(), $decoded);
+    }
+
+    /**
+     * @test
+     */
+    public function ItShouldReturnUnfilteredFieldsIfParamIsEmptyString()
+    {
+        $models = TestModel::all();
+        $decoded = $this->callEndpoint(['filter' => '']);
+
+        $this->assertEquals($models->toArray(), $decoded);
+    }
+
+    /**
+     * @test
+     */
+    public function ItShouldReturnUnfilteredFieldsIfParamIsNull()
+    {
+        $models = TestModel::all();
+        $decoded = $this->callEndpoint(['filter' => null]);
+
+        $this->assertEquals($models->toArray(), $decoded);
+    }
+
+}
